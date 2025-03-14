38134c34868a02ed9febef23d4f41248a82f1c83
Himani-Trivedi <himanitrivedi1874@gmail.com>
2025-03-14 22:41:28 +0530

    changes into API endpoints

--- src/main/java/com/example/FirstApp/contoller/CourseController.java
+++ src/main/java/com/example/FirstApp/contoller/CourseController.java
@@ -14,7 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@RequestMapping("/courses")
+@RequestMapping("/admin/courses")
 public class CourseController {
 
     @Autowired
--- src/main/java/com/example/FirstApp/contoller/DemoController.java
+++ src/main/java/com/example/FirstApp/contoller/DemoController.java
@@ -6,18 +6,27 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Getter
 @Setter
 @Data
 @Slf4j
+@RestController
 public class DemoController {
 
     @PostMapping("/")
     public String get(@RequestBody CourseDTO courseDTO){
         log.info(courseDTO.getCourseName());
         return courseDTO.getCourseName();
+    }
+
+    @GetMapping("/demo")
+    public String demo(){
+        log.info("inside demo");
+        return "This is demo public access";
     }
 }
